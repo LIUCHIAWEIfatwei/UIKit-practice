02c77a98547ee0aedc24f14e6c8d11a41b735919
LIUCHIAWEIfatwei <fattyboobootw@gmail.com>
2018-11-29 15:46:16 +0800

    Initial Commit

---
+++ UITableView_practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableView_practice
+//
+//  Created by Lavatar on 2018/11/29.
+//  Copyright © 2018 chiaWei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
