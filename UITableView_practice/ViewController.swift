16bf2ca9c6136a012b94cfd815b7d65d7007929d
LIUCHIAWEIfatwei <fattyboobootw@gmail.com>
2018-11-29 15:59:26 +0800

    # 設置UITableView. 並設置原點&尺寸.

--- UITableView_practice/ViewController.swift
+++ UITableView_practice/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var info : [[String]] = [["林書豪", "陳信安"], ["陳偉殷","王建民","王大明","葉大雄"]]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let fullScreen = UIScreen.main.bounds.size
+        
+        //設置UITableView. 並設置原點&尺寸.
+        let myTableView = UITableView(frame: CGRect(x: 0, y: 20, width: fullScreen.width , height: fullScreen.height - 20), style: .grouped)
+        
+        
+        
+        
+        
     }
 
 
