c72a078aaeac5b9a4f93cda385b7efc39d822433
LIUCHIAWEIfatwei <fattyboobootw@gmail.com>
2018-11-29 16:07:43 +0800

    # 註冊 cell

--- UITableView_practice/ViewController.swift
+++ UITableView_practice/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
     
     var info : [[String]] = [["林書豪", "陳信安"], ["陳偉殷","王建民","王大明","葉大雄"]]
 
@@ -19,10 +21,27 @@ class ViewController: UIViewController {
         //設置UITableView. 並設置原點&尺寸.
         let myTableView = UITableView(frame: CGRect(x: 0, y: 20, width: fullScreen.width , height: fullScreen.height - 20), style: .grouped)
         
+        //註冊 cell
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        //
+        
+        //設置委任對象
+        myTableView.dataSource = self
+        myTableView.delegate = self
         
         
         
         
+    }
+    
+    //
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
 
 
